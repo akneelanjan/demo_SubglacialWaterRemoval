7f3c947f7c557b39618c3c784a53545d8d807cf8
NEELANJAN AKULI <akneelanjan@gmail.com>
2026-05-10 20:16:45 -0700

    Delete smearedHeavi.m

--- smearedHeavi.m
+++
@@ -1,7 +0,0 @@
-function Y = smearedHeavi(X,eps)
-%Builds smeared Heaviside function (0-to-1) with smearing band eps
-
-Y = zeros(size(X));
-Y(X >= 0) = 0.5 + (X(X >= 0)-(eps/2))/(eps) + ...
-               sin(2*pi*(X(X >= 0)-(eps/2))/eps)/(2*pi);
-Y(X > eps) = 1;

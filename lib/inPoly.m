4bea1e547650ef6f544aa91525a87dd3de2b30b6
NEELANJAN AKULI <akneelanjan@gmail.com>
2026-05-10 20:17:45 -0700

    Delete lib directory

--- lib/inPoly.m
+++
@@ -1,8 +0,0 @@
-function [boo] = inPoly(x,y,S)
-    % Function to use create mask for region inside a polygon S
-    n = size(S,1);
-    boo = false(size(x));
-    for i = 1:n
-        boo = max(boo, inpolygon(x,y,S(i).X,S(i).Y));
-    end
-end
--- lib/setFontSize.m
+++
@@ -1,4 +0,0 @@
-function setFontSize(fontSize)
-% Set font size for a figure
-set(findall(gcf,'-property','FontSize'),'FontSize',fontSize);
-end
